fe73a5dbcfc2ab7854c446563399d3729ff684dc
hvelarderiquelme <hvelardeab@gmail.com>
2025-03-08 08:07:29 -0700

    wdd131 personal homepage week1

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+// const currentYear = new Date();
+// let year = currentYear.getFullYear();
+// document.getElementById("currentyear").innerHTML = year;
+
+// const date = new Date(document.lastModified);
+// document.getElementById("lastModified").innerHTML = date;
+
+
