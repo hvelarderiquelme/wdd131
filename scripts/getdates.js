b21f4dda8f88f7297525c34ee3ee65a295a35f19
hvelarderiquelme <hvelardeab@gmail.com>
2025-03-17 17:04:50 -0600

    JS code for dates in footer

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,8 +1,13 @@
-// const currentYear = new Date();
-// let year = currentYear.getFullYear();
-// document.getElementById("currentyear").innerHTML = year;
+//Footer copyright and last modified information
+const ElmtYear = document.querySelector("#current-year");
+const ElmtLastModified = document.querySelector("#last-modified");
+const currentYear = new Date();
+const lastModified = document.querySelector("#last-modified");
+const date = new Date(document.lastModified);
+let year = currentYear.getFullYear();
+const spanElmt = document.createElement("span");
 
-// const date = new Date(document.lastModified);
-// document.getElementById("lastModified").innerHTML = date;
-
-
+document.querySelector('#current-year').innerHTML = '&COPY;' +  year + '&#x1F3B8;Horacio Velarde Riquelme &#x1F3B8; Canada';
+document.querySelector('#last-modified').innerHTML = date;
+ElmtYear.appendChild(spanElmt);
+ElemtLastModified.appendChild(spanElmt);          
--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,4 @@
+//Hamburger button responsive code
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('nav');
 
@@ -5,3 +6,18 @@ hamButton.addEventListener('click', () =>{
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
 });
+           
+
+//Footer copyright and last modified information
+const ElmtYear = document.querySelector("#current-year");
+const ElmtLastModified = document.querySelector("#last-modified");
+const currentYear = new Date();
+const lastModified = document.querySelector("#last-modified");
+const date = new Date(document.lastModified);
+let year = currentYear.getFullYear();
+const spanElmt = document.createElement("span");
+
+document.querySelector('#current-year').innerHTML = '&COPY;' +  year + '&#x1F3B8;Horacio Velarde Riquelme &#x1F3B8; Canada';
+document.querySelector('#last-modified').innerHTML = date;
+ElmtYear.appendChild(spanElmt);
+ElemtLastModified.appendChild(spanElmt);          
